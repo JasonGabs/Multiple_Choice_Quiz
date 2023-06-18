a53aea35124729ca5d70769dc4d4ef521be7c376
Jason <jasongabs@gmail.com>
2023-06-17 21:30:26 -0700

    Added framework for the quiz

---
+++ Develop/script.js
@@ -0,0 +1,32 @@
+var button1 = document.querySelector("#button1");
+var button2 = document.querySelector("#button2");
+var button3 = document.querySelector("#button3");
+var button4 = document.querySelector("#button4");
+
+var question1 = "How many feet are in 1500 meters?";
+var question2 = "What is the second tallest mountain in the world?";
+var question3 = "What is the answer to this math problem: 40 + 23 - 83 * 0 + 200 + 49 - 51?"
+var wrongAnswerMsg = "Unfortunately that's the wrong answer!"
+
+var allQuestions = [question1, question2, question3]
+
+var activeQuestion = 0;
+var correctButton = false;
+
+var element = document.getElementsById()
+function nextQuestion {
+    activeQuestion = allQuestions[0];
+    if (allQuestions.includes(question1)) {
+        button1 = document.addEventListener("click", b1Click) {
+            wrongAnswerMsg = element.innerhtml
+    }} else if {
+
+    } else if {
+
+    } else {
+
+    }
+        
+    }
+
+}
