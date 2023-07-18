8409ba656d231b752538c2138a4790217bbd54e9
Jason <jasongabs@gmail.com>
2023-07-17 18:10:53 -0700

    Finished topics and added fuctionality

--- Develop/script.js
+++ Develop/script.js
@@ -4,10 +4,8 @@
 // const fourthButton = document.querySelector("#button4");
 var startGameButton = document.getElementById("startGameButton");
 var timer = document.getElementById("timer");
-var scoresList = document.querySelector("#scoresList");
-var gameOver = document.getElementById("#gameOverScreen")
+var scoresList = document.querySelector("#scoresListButton");
 var mainQuestion = document.querySelector("#question1");
-// var dataIndex = document.querySelector("")
 var allQuestions = [
     {
         question: "How many feet are in 1500 meters?",
@@ -25,7 +23,7 @@ var allQuestions = [
         answer: "0",
     },
     {
-        question: "Game Over!",
+        question: "Game Over! Enter Initials For Your Score Then Press Enter!",
         options: []
 
     },
@@ -33,19 +31,25 @@ var allQuestions = [
 var currentIndex = 0;
 var storedScores = [];
 
+var correctAnswers = 0;
+
 var timeLeft = 30;
 timer.textContent = timeLeft + " seconds remaining";
+
+var initialsValue = null;
 
 startGameButton.addEventListener("click", function () {
     timerCountdown();
     updateQuestionText();
-    startGameButton.style.display = "none";
+    clear();
+    scoresList.style.display = "none";
 })
 
 
 function updateQuestionText() {
-    
+
         document.querySelector("#buttonArea").textContent = "";
+        mainQuestion.style.display = "";
         mainQuestion.textContent = allQuestions[currentIndex].question;
         for (var i = 0; i < allQuestions[currentIndex].options.length; i++) {
             var questionButton = document.createElement("button");
@@ -55,20 +59,20 @@ function updateQuestionText() {
             document.querySelector("#buttonArea").appendChild(questionButton);
             questionButton.addEventListener("click", function (event) {
                 var element = event.target;
-                if (element === allQuestions[currentIndex].answer) {
+                if (element.textContent === allQuestions[currentIndex].answer) {
                     nextQuestion();
+                    correctAnswers++;
                 } else {
                     nextQuestion();
                     timeLeft = timeLeft - 5;
                 }
             })
-            console.log(allQuestions[currentIndex].options)
         }
         
 }
 
 function nextQuestion () {
-    if (allQuestions[currentIndex].question === "Game Over!") {
+    if (allQuestions[currentIndex].question === "Game Over! Enter Initials For Your Score Then Press Enter!") {
         return
     }
     currentIndex++;
@@ -77,234 +81,113 @@ function nextQuestion () {
 
 
 
+
 function timerCountdown () {
 timeLeft = 30;
 timeLeft--;
 var timeInterval = setInterval(function () {
-    if (allQuestions[currentIndex].question === "Game Over!") {
-        timer.textContent = timeLeft;
+    if (allQuestions[currentIndex].question === "Game Over! Enter Initials For Your Score Then Press Enter!") {
+        timer.textContent = timeLeft + " Your Time Score";
+        
         document.querySelector("#buttonArea").textContent = "";
-        startGameButton.style.display = "";
-        startGameButton.textContent = "Play Again?";
+
         currentIndex = 0;
+
         clearInterval(timeInterval);
+
+        var score = document.getElementById("score");
+        score.textContent = correctAnswers + "/3 Correct Answers";
+        score.style.display = "";
+        getUserInitials();
+
+        
+     
     } else if (timeLeft > 1) {
-      timer.textContent = timeLeft + ' seconds remaining';
-      timeLeft--;
+        timer.textContent = timeLeft + ' seconds remaining';
+        timeLeft--;
 
     } else if (timeLeft === 1) {
-      timer.textContent = timeLeft + ' second remaining';
-      timeLeft--;
+        timer.textContent = timeLeft + ' second remaining';
+        timeLeft--;
 
     } else {
-      timer.textContent = "Times Up!";
-      mainQuestion.textContent = allQuestions[allQuestions.length - 1].question;
-      document.querySelector("#buttonArea").textContent = "";
-      startGameButton.style.display = "";
-      startGameButton.textContent = "Play Again?";
-      currentIndex = 0;
-      clearInterval(timeInterval);
+        timer.textContent = "Times Up!";
+        mainQuestion.textContent = allQuestions[allQuestions.length - 1].question;
+      
+        document.querySelector("#buttonArea").textContent = "";
+      
+        currentIndex = 0;
+
+        clearInterval(timeInterval);
+
+        var score = document.getElementById("score");
+        score.textContent = correctAnswers + "/3 Correct Answers";
+        getUserInitials();
+
     }
   }, 1000);
 }
 
-// questionButton.setAttribute("data-index", i);
-// questionButton.id = i;
 
-// document.querySelector()
-        // questionButton.addEventListener = ("click", function (event) {
-        //     var element = event.target;
-        //     if (element === allQuestions[currentIndex].answer) {
-        //         nextQuestion();
-        //     } else {
-        //         nextQuestion();
-        //         timeLeft = timeLeft - 5
-        //     }
-        // })
-
-// for (var i = 0; i < storedScores.length; i++) {
-//     var currentScore = storedScores[i];
-
-//     var li = document.createElement("li");
-//     li.textContent = currentScore;
-//     li.setAttribute("data-index", i);
-
-//     scoresList.appendChild(li);
-//   }
-  
-//   scoreButton.addEventListener("click", function(event) {
-//     var element = event.target;
-  
-//     // Checks if element is a button
-//     if (element.matches("button") === true) {
-//       // Get its data-index value and remove the todo element from the list
-//       var index = element.parentElement.getAttribute("data-index");
-//       todos.splice(index, 1);
-  
-//       // Store updated todos in localStorage, re-render the list
-//       renderScoresList();
-//     }
-// });
-
-// function saveToLocalStorage () {
-//     localStorage.setItem("Stored Scores", JSON.stringify(storedScores));
-
-// }
-// function renderScoresList () {
-
-// }
-
-// if (questionButton.onclick != allQuestions[currentIndex].answer) { 
-            //     timeLeft = timeLeft - 5;
-            //     nextQuestion();
-            // } else {
-            //     nextQuestion
-            // console.log(questionButton)
-            // console.log(questionButton.textContent)
-
-// function playAgain () {
-//     if (allQuestions[currentIndex].question = "Game Over!") {
-//         var playAgainButton = document.createElement("button");
-//         document.querySelector("#buttonArea").appendChild(playAgainButton);
-//         playAgainButton.textContent = "Play Again?";
-        
-//         playAgainButton.onclick = function () {
-//             timerCountdown();
-//             updateQuestionText();
-//             startGameButton.style.display = "none";
-//         }
-//     }
-// }
-
-
-// function hideElements () {
-//     firstButton.style.display = "none";
-//     secondButton.style.display = "none";
-//     thirdButton.style.display = "none";
-//     fourthButton.style.display = "none";
-//     gameOverText.style.display = "none";
-// }
-// function showQuestions() {
-//    firstButton.style.display = "inline-flex";
-//    secondButton.style.display = "inline-flex";
-//    thirdButton.style.display = "inline-flex";
-//    fourthButton.style.display = "inline-flex";
-//    startGameButton.style.display = "none";
-// }
-
-// This fuction iterates to the next question when it's called
-
-
-// function secondQuestion() {
-//     if (activeQuestion = allQuestions[1]) {
-//         firstButton = document.addEventListener("click", function () {
-//             allQuestions++;
-//         });
-            
-//         secondButton = document.addEventListener("click", function () {
-//             allQuestions++;
-//         });
-            
-//         thirdButton = document.addEventListener("click", function () {
-//             allQuestions++;
-//         });
-            
-//         fourthButton = document.addEventListener("click", function () {
-//             allQuestions++;
-//         }); 
-           
-//     }
-// }
-
-// function thirdQuestion() {
-//     if (activeQuestion = allQuestions[2]) {
-//         firstButton = document.addEventListener("click", function () {
-//             allQuestions++;
-//         });
-            
-//         secondButton = document.addEventListener("click", function () {
-//             allQuestions++;
-//         });
-            
-//         thirdButton = document.addEventListener("click", function () {
-//             allQuestions++;
-//         });
-            
-//         fourthButton = document.addEventListener("click", function () {
-//             allQuestions++;
-//         }); 
-           
-//     }
-// }
-// firstButton = document.addEventListener("click", function () {
-        //     firstButton.textContent = allQuestions[currentIndex];
-        //     currentIndex++;
-        //     questionTest();
-        // });
-            
-        // secondButton = document.addEventListener("click", function () {
-        //     allQuestions++;
-        // });
-            
-        // thirdButton = document.addEventListener("click", function () {
-        //     allQuestions++;
-        // });
-            
-        // fourthButton = document.addEventListener("click", function () {
-        //     allQuestions++;
-        // }); 
-    // }
-    // if (activeQuestion = allQuestions[0]) {
-        // firstButton = document.addEventListener("click", function () {
-        //     allQuestions++;
-        // });
-            
-        // secondButton = document.addEventListener("click", function () {
-        //     allQuestions++;
-        // });
-            
-        // thirdButton = document.addEventListener("click", function () {
-        //     allQuestions++;
-        // });
-            
-        // fourthButton = document.addEventListener("click", function () {
-        //     allQuestions++;
-        // }); 
-           
-    // }
-// if (activeQuestion = allQuestions[3]) {
-
-// }
-
-// function setScores() {
-//     win.textContent = winCounter;
-//     localStorage.setItem("scores", scores);
-//   }
-
-//   function getScores() {
-//     var storedScores = localStorage.getItem("scores");
-//     if (storedScores === null) {
-//        scoreArray = [];
-//     } else {
-//       scoreArray = [storedScores];
-//     }
-//     // win.textContent = winCounter;
-//   }
-//   function countdown() {
-//     var timeCount = 60;
-//     timeCount.textContent = timer;
-//     var timeInterval = setInterval(function () {
-//       if (timeCount > 1) {
-//         timer.textContent = timeCount + ' seconds remaining';
-//         timeCount--;
-//       } else if (timeCount === 1) {
-//         timer.textContent = timeCount + ' second remaining';
-//         timeCount--;
-//       } else {
-//         timer.textContent = '';
-//       }
-      
-//     },1000)
-// }
-
+scoresList.addEventListener("click", function () {
+    viewPastScores();
+    clear();
+});
+document.getElementById("reset").addEventListener("click", function () {
+    window.location.reload();
+});
+function viewPastScores() {
+    timer.textContent = "";
+    document.querySelector("#buttonArea").innerHTML = "";
+    startGameButton.style.display = "none";
     
+    mainQuestion.innerHTML = "";
+    currentIndex = 0;
+    var scoresSheet = document.getElementById("scoreSheet");
+    var scoresSheetArrayValue = JSON.parse(localStorage.getItem("Stored Scores"));
+    if (scoresSheetArrayValue !== "") {
+        for (var i = 0; i < scoresSheetArrayValue.length; i++) {
+            var element = document.createElement("p");
+            element.textContent = scoresSheetArrayValue[i].name + ", " + scoresSheetArrayValue[i].time + " Time Left, " + scoresSheetArrayValue[i].score + "/3 Questions Correct";
+            scoresSheet.appendChild(element);
+        }
+    } 
+}
+
+function saveToLocalStorage () {
+    
+    const oldScores = JSON.parse(localStorage.getItem("Stored Scores")) || []
+    var highScores = {
+        name: initialsValue,
+        score: " " + correctAnswers,
+        time: " " + timeLeft,
+    }
+    oldScores.push(highScores);
+    console.log(oldScores);
+    localStorage.setItem("Stored Scores", JSON.stringify(oldScores));
+    
+}
+
+function clear() {
+    score.style.display = "none";
+    startGameButton.style.display = "none";
+}
+
+function getUserInitials() {
+    var userInitials = document.createElement("input");
+    var quizArea = document.getElementById("quizArea");
+    quizArea.appendChild(userInitials);
+    userInitials.addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
+            initialsValue = userInitials.value
+            saveToLocalStorage();
+            clear();
+            correctAnswers = 0;
+            quizArea.removeChild(userInitials);
+            mainQuestion.style.display = "none";
+            startGameButton.style.display = "";
+            startGameButton.textContent = "Play Again?";
+            scoresList.style.display = "";
+        }
+})   
+}
